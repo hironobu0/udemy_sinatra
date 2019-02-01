beaa52ee4e84c63cd9b09c651d75d3f0fe0faec7
hironobu0 <h.ohashi6491@gmail.com>
2019-02-01 17:58:11 +0900

    first commit

---
+++ myapp.rb
@@ -0,0 +1,20 @@
+# myapp.rb
+require 'rubygems'
+require 'bundler'
+
+
+Bundler.require
+get '/' do
+  @now = Time.now
+  erb :index
+end
+
+get '/contact_new' do
+  erb :contact_form
+end
+
+post '/contacts' do
+  puts '###送信されたデータ###'
+  p params
+  redirect '/'
+end
