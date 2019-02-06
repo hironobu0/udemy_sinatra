61b2795b66c3dab7ef46107762eb8827ae96e6a3
hironobu0 <h.ohashi6491@gmail.com>
2019-02-06 19:08:14 +0900

    Complete tutrial

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_01_093335) do
+ActiveRecord::Schema.define(version: 2019_02_05_094357) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
+    t.string "email"
   end
 
 end
--- myapp.rb
+++ myapp.rb
@@ -6,32 +6,37 @@ require 'bundler'
 Bundler.require
 
 set :database, {adapter: "sqlite3", database: "contacts.sqlite3"}
+enable :sessions
 
-class Contact < ActiveRecord::Base
+class Contact < ActiveRecord::Base   # DBにアクセスするクラス？
   validates_presence_of :name
+  validates_presence_of :email
 end
 
-get '/' do                  # ホーム画面を表示
-  @now = Time.now           # index.erbに渡す @を付ける
-  @contacts = Contact.all   # @contactsに配列としてDB内のレコードを追加
+get '/' do                    # ホーム画面を表示
+  @now      = Time.now        # index.erbに渡す @を付ける
+  @contacts = Contact.all     # @contactsに配列としてDB内のレコードを追加
+  @message  = session.delete :message
   erb :index
 end
 
-get '/contact_new' do       # 新規連絡先作成画面を表示
-  @contact = Contact.new    # 
+get '/contact_new' do         # 新規連絡先作成画面を表示
+  @contact = Contact.new      # 新しくレコードを作成
   erb :contact_form
 end
 
-post '/contacts' do
+post '/contacts' do           # viewのcontact_formから送信されてくる
   puts '###送信されたデータ###'
-  p params
+  p params                    # paramsの中身を表示
 
-  name = params[:name]
-
+  disp_name = params[:input_name]  # viewでフォームからPOST送信された<input name = "name">の情報を受け取る
+                                # フォームの内容が:input_nameの中身に入る
   # DBに保存
-  @contact = Contact.new({name: name})
-  if @contact.save
+  @contact = Contact.new( { name: disp_name, email: params[:input_email] } ) # nameカラムにnamingのデータを保存
+                                            # {name: disp_name}は{:name => disp_name}と同一
+  if @contact.save                          # ActiveRecordのsaveメソッド？
     # true
+    session[:message] = "#{disp_name}さんを作成しました"
     redirect '/'
   else
     # false
